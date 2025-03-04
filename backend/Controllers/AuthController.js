31b46ca43104cd09334119923824e6012bdc85f9
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-03-04 07:59:53 +0530

    jwt,signup,login setup

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,15 +1,62 @@
-
+const brcypt = require('bcrypt');
+const UserModel = require('../Models/user.model');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res) => {
   try{
-
+    const { name, email, password } = req.body;
+    const user = await UserModel.findOne({ email});
+    if(user) {
+      return res.status(409)
+        .json({message: "User already exists", success: false});
+    }
+    const userModel = new UserModel({name, email, password});
+    userModel.password = await brcypt.hash(password, 10);
+    await userModel.save();
+    return res.status(201).json({message: "User created", success: true});
   }
   catch {err} {
+    return res.status(500).json({message: "Internal server error", success:false});
+  }
+};
 
+const login = async (req, res) => {
+  try{
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email});
+    const errorMsg = "Auth failed, email or password is wrong";
+    if(!user) {
+      return res.status(403)
+        .json({message: errorMsg, success: false});
+    }
+    const isPasswordValid = await brcypt.compare(password, user.password);
+    if(!isPasswordValid) {
+      return res.status(403)
+        .json({message: errorMsg, success: false});
+  }
+  const jwtToken = jwt.sign({
+    email: user.email,
+    userId: user._id
+  },
+  process.env.JWT_SECRET,
+  {
+    expiresIn: "24h"
+  });
+  return res.status(200).json({message: "Login successful", success: true,
+    jwtToken, 
+    email,
+    name: user.name,
+    userId: user._id
+  });
+}
+  catch {err} {
+    return res.status(500).json({message: "Internal server error", success:false});
   }
 };
 
 
+
 module.exports = {
-  signup
+  signup,
+  login
 };
--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const { signupValidation } = require('../Middlewares/AuthValidation');
 const router = express.Router();
+const { signup } = require('../Controllers/AuthController');
+const { login } = require('../Controllers/AuthController');
+const { loginValidation } = require('../Middlewares/AuthValidation');
 
-router.post("/login", (req, res) => {
-  res.send("login success");
-});
 
-router.post("/signup", (req, res) => {
-  res.send("signup success");
-});
+router.post("/login", loginValidation, login);
+
+router.post("/signup", signupValidation, signup);
 
 module.exports = router;
